197ae59081e9640de0e34ae30de934bffdd9f231
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-10-19 23:07:48 -0500

    added scanner test

---
+++ scanner_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestScannerCodeAndFields(t *testing.T) {
+	testCases := map[string][]struct {
+		Code   string
+		Fields []string
+	}{
+		"empty": {},
+		"simple": {
+			{"1", []string{"these", "are", "the", "fields"}},
+			{"0", []string{"more", "fields"}},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// build test text
+			var builder strings.Builder
+			for _, r := range tc {
+				builder.WriteString(r.Code + strings.Join(r.Fields, "\t") + "\n")
+			}
+			builder.WriteString(".\n")
+
+			scanner := NewScanner(bytes.NewBufferString(builder.String()))
+
+			for _, r := range tc {
+				if !scanner.Scan() {
+					t.Fatal("failed to scan code")
+				}
+				if scanner.Code() != r.Code || !reflect.DeepEqual(scanner.Fields(), r.Fields) {
+					t.Fatal("failed to scan fields")
+				}
+			}
+		})
+	}
+}
